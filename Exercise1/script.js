8c48804d0135d165b68ec1520b0072f6f28f40d4
Skjalgn <skjalgn@gmail.com>
2023-05-31 14:15:25 +0200

    ganske ferdig

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -124,195 +124,187 @@ function createCanvas() {
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+var isDay = true;
 
 function createSVG() {
     const svg = document.getElementById('svg');
+    while (svg.firstChild) {
+        svg.removeChild(svg.firstChild);
+    }
     const width = svg.width.baseVal.value;
     const height = svg.height.baseVal.value;
-    var angle = 0;
 
-    var skycolor = 'paleturquoise';
-    var sunColor = 'yellow';
-    var houseColor = 'orangered';
-    var woodColor = 'sienna';
-    var grassColor = 'greenyellow';
-    var windowColor = 'white';
-    var isDay = true;
+    if (!isDay) {
+        skycolor = 'darkblue';
+        sunColor = 'white';
+        houseColor = 'darkred';
+        woodColor = 'saddlebrown';
+        grassColor = 'darkgreen';
+        windowColor = 'yellow';
+        isDay = false;
 
-    $(document).ready(function () {
-        $('#svg').click(function (event) {
-            console.log('tete');
-            var rect = svg.getBoundingClientRect();
-            var mouseX = event.clientX - rect.left;
-            var mouseY = event.clientY - rect.top;
-            if (mouseX > 400 && mouseX < 450 && mouseY > 30 && mouseY < 80 && isDay) {
-                console.log('gege');
-                skycolor = 'darkblue';
-                sunColor = 'white';
-                houseColor = 'darkred';
-                woodColor = 'saddlebrown';
-                grassColor = 'darkgreen';
-                windowColor = 'yellow';
-                isDay = false;
-            
-            }
-            else if (mouseX > 400 && mouseX < 450 && mouseY > 30 && mouseY < 80 && !isDay) {
-                skycolor = 'paleturquoise';
-                sunColor = 'yellow';
-                houseColor = 'orangered';
-                woodColor = 'sienna';
-                grassColor = 'greenyellow';
-                windowColor = 'white';
-                isDay = true;
-        
-            }
-        })
+    }
+    else {
+        skycolor = 'paleturquoise';
+        sunColor = 'yellow';
+        houseColor = 'orangered';
+        woodColor = 'sienna';
+        grassColor = 'greenyellow';
+        windowColor = 'white';
+        isDay = true;
+    }
+
+
+    //the grass
+    var grass = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    grass.setAttribute("x", 0);
+    grass.setAttribute("y", 0);
+    grass.setAttribute("width", width);
+    grass.setAttribute("height", height);
+    grass.setAttribute("fill", grassColor);
+    svg.appendChild(grass);
+
+    //the sky
+    var sky = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    sky.setAttribute("x", 0);
+    sky.setAttribute("y", 0);
+    sky.setAttribute("width", width);
+    sky.setAttribute("height", height / 2);
+    sky.setAttribute("fill", skycolor);
+    svg.appendChild(sky);
+
+    //the sun
+    const centerX = width * 6 / 7;
+    const centerY = height / 8;
+    const radius = 20;
+    var sun = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    sun.setAttribute("cx", centerX);
+    sun.setAttribute("cy", centerY);
+    sun.setAttribute("r", radius);
+    sun.setAttribute("fill", sunColor);
+    svg.appendChild(sun);
+
+    $(sun).on("click", function () {
+        // Perform the desired action here
+        if (isDay) {
+            isDay = false;
+        }
+        else {
+            isDay = true;
+        }
+        createSVG();
     });
 
-    function updateSVG() {
-        svg.innerHTML = '';
 
-        //the grass
-        var grass = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        grass.setAttribute("x", 0);
-        grass.setAttribute("y", 0);
-        grass.setAttribute("width", width);
-        grass.setAttribute("height", height);
-        grass.setAttribute("fill", grassColor);
-        svg.appendChild(grass);
+    //the house
+    var house = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    house.setAttribute("x", 310);
+    house.setAttribute("y", 180);
+    house.setAttribute("width", 130);
+    house.setAttribute("height", 60);
+    house.setAttribute("fill", houseColor);
+    svg.appendChild(house);
 
-        //the sky
-        var sky = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        sky.setAttribute("x", 0);
-        sky.setAttribute("y", 0);
-        sky.setAttribute("width", width);
-        sky.setAttribute("height", height / 2);
-        sky.setAttribute("fill", skycolor);
-        svg.appendChild(sky);
+    //the roof
+    var roof = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    roof.setAttribute("points", "300,180 370,140 450,180");
+    roof.setAttribute("fill", woodColor);
+    svg.appendChild(roof);
 
-        //the sun
-        const centerX = width * 6 / 7;
-        const centerY = height / 8;
-        const radius = 20;
-        var sun = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        sun.setAttribute("cx", centerX);
-        sun.setAttribute("cy", centerY);
-        sun.setAttribute("r", radius);
-        sun.setAttribute("fill", sunColor);
-        svg.appendChild(sun);
+    //the door
+    var door = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    door.setAttribute("x", 350);
+    door.setAttribute("y", 200);
+    door.setAttribute("width", 20);
+    door.setAttribute("height", 40);
+    door.setAttribute("fill", woodColor);
+    svg.appendChild(door);
 
-        //the house
-        var house = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        house.setAttribute("x", 310);
-        house.setAttribute("y", 180);
-        house.setAttribute("width", 130);
-        house.setAttribute("height", 60);
-        house.setAttribute("fill", houseColor);
-        svg.appendChild(house);
+    //the window
+    var window = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    window.setAttribute("x", 400);
+    window.setAttribute("y", 200);
+    window.setAttribute("width", 20);
+    window.setAttribute("height", 20);
+    window.setAttribute("fill", windowColor);
+    svg.appendChild(window);
 
-        //the roof
-        var roof = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
-        roof.setAttribute("points", "300,180 370,140 450,180");
-        roof.setAttribute("fill", woodColor);
-        svg.appendChild(roof);
+    //windmill
+    var ceX = 150;
+    var ceY = 120;
+    var length = 75;
+    var endX1 = ceX - length;
+    var endY1 = ceY;
+    var endX2 = ceX + length;
+    var endY2 = ceY;
+    var endX3 = ceX;
+    var endY3 = ceY - length;
+    var endX4 = ceX;
+    var endY4 = ceY + length;
 
-        //the door
-        var door = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        door.setAttribute("x", 350);
-        door.setAttribute("y", 200);
-        door.setAttribute("width", 20);
-        door.setAttribute("height", 40);
-        door.setAttribute("fill", woodColor);
-        svg.appendChild(door);
+    // Draw the rotating line
+    var line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line1.setAttribute("x1", endX1);
+    line1.setAttribute("y1", endY1);
+    line1.setAttribute("x2", endX2);
+    line1.setAttribute("y2", endY2);
+    line1.setAttribute("stroke", woodColor);
+    line1.setAttribute("stroke-width", 5);
+    svg.appendChild(line1);
 
-        //the window
-        var window = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-        window.setAttribute("x", 400);
-        window.setAttribute("y", 200);
-        window.setAttribute("width", 20);
-        window.setAttribute("height", 20);
-        window.setAttribute("fill", windowColor);
-        svg.appendChild(window);
+    animateWindmillBlades(line1, 0);
 
-        //windmill
-        var ceX = 150;
-        var ceY = 120;
-        var length = 75;
-        var endX1 = ceX + length * Math.cos(angle);
-        var endY1 = ceY + length * Math.sin(angle);
-        var endX2 = ceX + length * Math.cos(angle + Math.PI / 2);
-        var endY2 = ceY + length * Math.sin(angle + Math.PI / 2);
-        var endX3 = ceX + length * Math.cos(angle + Math.PI);
-        var endY3 = ceY + length * Math.sin(angle + Math.PI);
-        var endX4 = ceX + length * Math.cos(angle + 3 * Math.PI / 2);
-        var endY4 = ceY + length * Math.sin(angle + 3 * Math.PI / 2);
+    var line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line2.setAttribute("x1", endX3);
+    line2.setAttribute("y1", endY3);
+    line2.setAttribute("x2", endX4);
+    line2.setAttribute("y2", endY4);
+    line2.setAttribute("stroke", woodColor);
+    line2.setAttribute("stroke-width", 5);
+    svg.appendChild(line2);
 
-        // Draw the rotating line
-        var line1 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line1.setAttribute("x1", ceX);
-        line1.setAttribute("y1", ceY);
-        line1.setAttribute("x2", endX1);
-        line1.setAttribute("y2", endY1);
-        line1.setAttribute("stroke", woodColor);
-        line1.setAttribute("stroke-width", 5);
-        svg.appendChild(line1);
+    animateWindmillBlades(line2, 0);
 
-        var line2 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line2.setAttribute("x1", ceX);
-        line2.setAttribute("y1", ceY);
-        line2.setAttribute("x2", endX2);
-        line2.setAttribute("y2", endY2);
-        line2.setAttribute("stroke", woodColor);
-        line2.setAttribute("stroke-width", 5);
-        svg.appendChild(line2);
+    var line3 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line3.setAttribute("x1", 175);
+    line3.setAttribute("y1", 300);
+    line3.setAttribute("x2", ceX);
+    line3.setAttribute("y2", ceY);
+    line3.setAttribute("stroke", woodColor);
+    line3.setAttribute("stroke-width", 5);
+    svg.appendChild(line3);
 
-        var line3 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line3.setAttribute("x1", ceX);
-        line3.setAttribute("y1", ceY);
-        line3.setAttribute("x2", endX3);
-        line3.setAttribute("y2", endY3);
-        line3.setAttribute("stroke", woodColor);
-        line3.setAttribute("stroke-width", 5);
-        svg.appendChild(line3);
+    var line4 = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line4.setAttribute("x1", ceX);
+    line4.setAttribute("y1", ceY);
+    line4.setAttribute("x2", 125);
+    line4.setAttribute("y2", 300);
+    line4.setAttribute("stroke", woodColor);
+    line4.setAttribute("stroke-width", 5);
+    svg.appendChild(line4);
+}
 
-        var line4 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line4.setAttribute("x1", ceX);
-        line4.setAttribute("y1", ceY);
-        line4.setAttribute("x2", endX4);
-        line4.setAttribute("y2", endY4);
-        line4.setAttribute("stroke", woodColor);
-        line4.setAttribute("stroke-width", 5);
-        svg.appendChild(line4);
+function animateWindmillBlades(blade, delay) {
+    const duration = 5; // Animation duration in seconds
 
-        var line5 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line5.setAttribute("x1", 175);
-        line5.setAttribute("y1", 300);
-        line5.setAttribute("x2", ceX);
-        line5.setAttribute("y2", ceY);
-        line5.setAttribute("stroke", woodColor);
-        line5.setAttribute("stroke-width", 5);
-        svg.appendChild(line5);
+    const animation = document.createElementNS("http://www.w3.org/2000/svg", "animateTransform");
+    animation.setAttribute("attributeName", "transform");
+    animation.setAttribute("attributeType", "XML");
+    animation.setAttribute("type", "rotate");
+    animation.setAttribute("from", "0 150 120");
+    animation.setAttribute("to", "360 150 120");
+    animation.setAttribute("dur", duration + "s");
+    animation.setAttribute("repeatCount", "indefinite");
+    animation.setAttribute("begin", delay + "s");
 
-        var line6 = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line6.setAttribute("x1", ceX);
-        line6.setAttribute("y1", ceY);
-        line6.setAttribute("x2", 125);
-        line6.setAttribute("y2", 300);
-        line6.setAttribute("stroke", woodColor);
-        line6.setAttribute("stroke-width", 5);
-        svg.appendChild(line6);
+    blade.appendChild(animation);
 
-        angle += 0.01;
-
-        requestAnimationFrame(updateSVG);
-    }
-    updateSVG();
 }
 
 function toggleInfoWindow() {
     const infoWindow = document.getElementById('info-window');
     infoWindow.style.display = infoWindow.style.display === 'flex' ? 'none' : 'flex';
-  }
+}
 
 createCanvas();
 createSVG();
